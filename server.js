3e2467992f47da6d51c90f3262e55f707d7a2136
g0rb <gabepostle@gmail.com>
2022-11-18 18:20:40 -0600

    i made a server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = process.argv[2] || 80
+
+const www = express.static(`${__dirname}/www`)
+app.use(www)
+
+app.listen(port, () => {
+    console.log(`running the server on port ${port}, visit http://localhost:${port}`)
+})
